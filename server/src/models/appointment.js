921adb83f30901377e191c519db57927ef5958e1
Arif Shahriyar Nader <arif.nader95@gmail.com>
2025-01-30 16:23:41 +0600

    Appointment collection created

---
+++ server/src/models/appointment.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const AppointmentSchema = new mongoose(
+  {
+    student: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    teacher: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    course: { type: String, required: true },
+    date: { type: Date, required: true },
+    timeSlot: { type: String, required: true },
+    status: {
+      type: String,
+      enum: ["pending", "approved", "rejected"],
+      default: "pending",
+    },
+    agenda: { type: String },
+  },
+  { timestamps: true }
+);
+
+export const Appointment = mongoose.model("Appointment", AppointmentSchema);
