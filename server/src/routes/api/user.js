be5f9d8999ba969c1ca800434034f19266039bd1
Arif Shahriyar Nader <arif.nader95@gmail.com>
2025-02-02 19:12:23 +0600

    Update user teacher and student

--- server/src/routes/api/user.js
+++ server/src/routes/api/user.js
@@ -149,19 +149,36 @@ router.put("/users/:id", authenticateToken, async (req, res) => {
   }
 });
 
-//delete user 
-router.delete("/users/:id", authenticateToken, async(req,res) =>{
-    try{
-        const user=await User.findByIdAndDelete(req.params.id);
-        if(!user){
-            return res.status(404).json({message:"User not found"});
-        }
-        res.status(200).json({message:"User deleted successfully"})
+//delete user
+router.delete("/users/:id", authenticateToken, async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
-    catch(error){
-        res.status(500).json({ message: "Server error", error });
+    res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error });
+  }
+});
+
+//update user
+router.put("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const userBody = req.body;
+    const updateUser = await User.findByIdAndUpdate(id, userBody, {
+      new: true,
+    });
+    if (updateUser) {
+      return res.status(200).json(updateUser);
+    } else {
+      return res.status(404).json({ message: "User not found" });
     }
-})
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error });
+  }
+});
 
 export default router;
 
