89eee2ed89ba592aa59ad068ddcbed6f5a1532b5
Arif Shahriyar Nader <arif.nader95@gmail.com>
2025-04-23 18:17:07 +0600

    CustomAlert added in SignIn component instead of js alert

--- client/src/components/sign-in/Signin.jsx
+++ client/src/components/sign-in/Signin.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { EyeIcon, EyeOffIcon } from "lucide-react";
 import { Link, useNavigate } from "react-router-dom";
 import { authServices } from "../../auth";
+import { CustomAlert } from "@/common/components";
 export const Signin = () => {
   const navigate = useNavigate();
   const [formData, setFormData] = useState({
@@ -9,6 +10,10 @@ export const Signin = () => {
     password: "",
   });
   const [showPassword, setShowPassword] = useState(false);
+  const [alertOpen, setAlertOpen] = useState(false);
+  const [alertMessage, setAlertMessage] = useState({
+    description: "",
+  });
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
@@ -24,19 +29,25 @@ export const Signin = () => {
 
     try {
       const authUser = await authServices.login(payload);
-
       if (!authUser || !authUser.user) {
         throw new Error("Invalid response from server");
       }
 
       const user = authUser.user;
-
       if (user.status === "pending") {
-        alert("Your registration is pending approval from the admin.");
+        setAlertMessage({
+          description: "Your registration is pending approval from the admin.",
+          variant: "default",
+        });
+        setAlertOpen(true);
         return;
       }
       if (user.status === "rejected") {
-        alert("Your registration has been rejected by the admin.");
+        setAlertMessage({
+          description: "Your registration has been rejected by the admin.",
+          variant: "destructive",
+        });
+        setAlertOpen(true);
         return;
       }
 
@@ -47,14 +58,24 @@ export const Signin = () => {
       } else if (user.role === "admin") {
         navigate("/admin-dashboard");
       } else {
-        alert("Login successful!");
+        setAlertMessage({
+          title: "Login Successful",
+          description: "Login successful!",
+          variant: "success",
+        });
+        setAlertOpen(true);
       }
     } catch (error) {
       console.error(
         "Login error:",
         error.response?.data?.message || error.message
       );
-      alert(error.response?.data?.message || "Unable to log in");
+
+      setAlertMessage({
+        description: error.response?.data?.message || "Unable to log in",
+        variant: "destructive",
+      });
+      setAlertOpen(true);
     }
   };
 
@@ -62,6 +83,11 @@ export const Signin = () => {
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
       <div className="bg-white p-6 rounded-lg shadow-md w-96">
         <h2 className="text-2xl font-bold mb-4 text-center">Login</h2>
+        <CustomAlert
+          open={alertOpen}
+          setOpen={setAlertOpen}
+          {...alertMessage}
+        />
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
             <label className="block text-gray-700 text-sm font-bold mb-2">
